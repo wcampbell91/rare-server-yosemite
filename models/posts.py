30d92d5746d5dd91d0b44109981f443c19f3a038
Joshua Medlen <joshua246@gmail.com>
2020-11-05 19:55:56 -0600

    work in progress

--- models/posts.py
+++ models/posts.py
@@ -1,12 +1,10 @@
-from datetime import date
-
 class Post():
      
-      def __init__(self, id, title, content, category_id, header_img, user_id):
+      def __init__(self, id, title, content, category_id, header_img, user_id, publish_date):
           self.id = id
           self.title = title
           self.content = content
           self.category_id = category_id
           self.header_img = header_img
           self.user_id = user_id
-          # self.publish_date = date.today()
+          self.publish_date = publish_date
--- posts/request.py
+++ posts/request.py
@@ -1,17 +1,18 @@
 import sqlite3
 import json
 from models import Post
+from datetime import datetime
 
 def create_post(new_post):
-    with sqlite3.connect('../rare.db') as conn:
+    with sqlite3.connect('./rare.db') as conn:
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
         INSERT INTO posts
-            (title, content, category_id, header_img)
+            (title, content, category_id, header_img, user_id, publish_date)
         VALUES
-            (?, ?, ?, ?)
-        """, (new_post['title'], new_post['content'], new_post['category_id'], new_post['header_img']))
+            (?, ?, ?, ?, ?, ?)
+        """, (new_post['title'], new_post['content'], new_post['category_id'], new_post['header_img'], new_post['user_id'], datetime.now()))
 
         id = db_cursor.lastrowid
         new_post['id'] = id
@@ -20,7 +21,7 @@ def create_post(new_post):
 
 
 def get_all_posts():
-    with sqlite3.connect('../rare.db') as conn:
+    with sqlite3.connect('./rare.db') as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
@@ -31,7 +32,8 @@ def get_all_posts():
           p.content,
           p.category_id,
           p.header_img,
-          p.user_id
+          p.user_id,
+          p.publish_date
         FROM posts p
         """)
 
@@ -40,7 +42,7 @@ def get_all_posts():
         dataset = db_cursor.fetchall()
 
         for row in dataset:
-          post = Post(row['id'], row['title'], row['content'], row['category_id'], row['header_img'], row['user_id'])
+          post = Post(row['id'], row['title'], row['content'], row['category_id'], row['header_img'], row['user_id'], row['publish_date'])
           posts.append(post.__dict__)
 
     return json.dumps(posts)
@@ -71,7 +73,7 @@ def get_posts_by_user(user_id):
     return json.dumps(posts)
 
 def get_single_post(id):
-    with sqlite3.connect('../rare.db') as conn:
+    with sqlite3.connect('./rare.db') as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
